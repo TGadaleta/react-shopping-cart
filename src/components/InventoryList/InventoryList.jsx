6493d4ae2910d89e6b5460520e7360572e47ef56
Tony Gadaleta <anthonygadaleta@gmail.com>
2024-12-09 16:45:26 -0500

    Finished App

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -1,19 +1,32 @@
 // src/components/InventoryList/InventoryList.jsx
+import './InventoryList.css'
 
-const InventoryList = ({inventory, title, handleAddItem}) => {
+const InventoryList = ({inventory, title, handleAddItem, handleRemoveItem}) => {
     return (
-      <div>
+      <div className='inventory-list'>
         <h2>{title}</h2>
         <ul>
-            {inventory.map((item) => (
+        {inventory.length ? (
+          inventory.map((item) => (
             <li key={item._id}>
-            <p>{item.name}</p>
-            <p>Price: {item.price}</p>
-            <button onClick={() => handleAddItem(item)}>
-                Add to Cart
-              </button>
+              <p>{item.name}</p>
+              <p>Price: {item.price}</p>
+              {handleAddItem ? (
+                <button onClick={() => handleAddItem(item)}>
+                  Add Item
+                </button>
+              ) : (
+                <button onClick={() => handleRemoveItem(item)}>
+                  Remove Item
+                </button>
+              )}
+            </li>
+          ))
+        ) : (
+          <li>
+            <p>Empty</p>
           </li>
-        ))}
+        )}
         </ul>
       </div>
     );
